c359a081d7b634c30fef250b4d8eb0a3611af890
AntonSV123 <sulak089@gmail.com>
2025-03-31 11:23:54 +0000

    харчування

--- src/pages/Nutrition.jsx
+++ src/pages/Nutrition.jsx
@@ -2,12 +2,12 @@ function Nutrition() {
   return (
     <main className="container px-4 py-4 flex-grow-1">
       <section>
-        <h2 className="h2 text-success mb-4">Харчування зайців</h2>
-        <p>Зайці є травоїдними тваринами. Їх раціон включає:</p>
+        <h2 className="h2 text-success mb-4">Харчування Рудих Кенгуру</h2>
+        <p>Харчування Рудих Кенгуру складається переважно з рослинної їжі, і вони добре пристосовані до виживання в суворих умовах. Їх раціон включає:</p>
         <ul className="list-group">
-          <li className="list-group-item">Траву, листя і кору дерев.</li>
-          <li className="list-group-item">Овочі, такі як морква та капуста.</li>
-          <li className="list-group-item">Воду, яку вони споживають у великих кількостях.</li>
+          <li className="list-group-item">Трави – основна їжа Рудих Кенгуру. Вони віддають перевагу зеленим травам і злаковим культурам.</li>
+          <li className="list-group-item">Листя чагарників – коли мало трав'янистої рослинності, Кенгуру можуть їсти листя деяких чагарників.</li>
+          <li className="list-group-item">Соковиті рослини – у посушливий період вони знаходять вологу в рослинах, таких як сукуленти.</li>
         </ul>
       </section>
     </main>
