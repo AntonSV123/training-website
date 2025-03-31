c6440773a73d7b2434bd4cdb2e03ab049858d934
AntonSV123 <sulak089@gmail.com>
2025-03-31 11:14:28 +0000

    фотер і хедер

--- src/components/layout/Header.jsx
+++ src/components/layout/Header.jsx
@@ -22,11 +22,11 @@ function Header() {
         <div className="logo-container">
           <Link to="/" className="text-white text-decoration-none d-flex align-items-center">
             <img 
-              src="/images/european-hare-6121jpg.webp" 
-              alt="Заєць" 
+              src="/images/a-red-kangaroo-in-the-sturt-stony-desert-australia.webp" 
+              alt="Рудий Кенгуру" 
               className="site-logo rounded-circle me-2"
             />
-            <span className="fs-4">Сайт про зайців</span>
+            <span className="fs-6">Сайт про Рудих Кенгуру</span>
           </Link>
         </div>
 
@@ -35,16 +35,16 @@ function Header() {
             Головна
           </Link>
           <Link to="/morphology" className={`nav-link text-white ${location.pathname === '/morphology' ? 'active' : ''}`}>
-            Зовнішній вигляд зайців
+            Зовнішній вигляд Рудих Кенгуру
           </Link>
           <Link to="/nutrition" className={`nav-link text-white ${location.pathname === '/nutrition' ? 'active' : ''}`}>
-            Харчування зайців
+            Харчування Рудих Кенгуру
           </Link>
           <Link to="/population" className={`nav-link text-white ${location.pathname === '/population' ? 'active' : ''}`}>
-            Ареал зайців
+            Ареал Рудих Кенгуру
           </Link>
           <Link to="/photo" className={`nav-link text-white ${location.pathname === '/photo' ? 'active' : ''}`}>
-            Фотографії зайців
+            Фотографії Рудих Кенгуру
           </Link>
         </nav>
 
